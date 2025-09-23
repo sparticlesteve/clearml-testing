d7aaa93ae9a162d4b3cca6e94c6835078e9bf313
Steve Farrell <sparticlesteve@gmail.com>
2025-09-22 21:05:08 -0700

    initial scripts

---
+++ hello_world.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Basic hello-world test of clearml
+echo "Hello world from a ClearML bash task!"
---
+++ submit_hello_world.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Use standard NERSC python environment
+module load python
+
+# Install clearml cli
+if ! pip show clearml > /dev/null 2>&1; then
+    echo "Installing clearml..."
+    pip install --no-cache-dir clearml
+else
+    echo "clearml is already installed."
+fi
+
+clearml-task \
+    --project clearml-testing \
+    --name hello-bash \
+    --queue muller \
+    --task-type custom \
+    --script hello_world.sh
