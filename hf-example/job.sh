c71d110e3f9d2d822eddc5916529d83cd43ca277
Steve Farrell <sparticlesteve@gmail.com>
2025-09-24 23:30:23 -0700

    update

--- hf-example/job.sh
+++ hf-example/job.sh
@@ -13,30 +13,34 @@ pip list
 # Update path to pickup torchrun command
 export PATH=../../../bin:$PATH
 
+# Python binary tests
+which python
+python -c "print('hello')"
+
 # Environment setup
 #module load pytorch/2.6.0
 
-# Use scratch for huggingface cache
-export HF_HOME=$SCRATCH/cache/huggingface
-
-# Distributed training configuration
-export MASTER_ADDR=$(scontrol show hostnames | head -n 1)
-export MASTER_PORT=29507
-
-env | grep MASTER
-
-# Other settings
-export OMP_NUM_THREADS=8
-#export NCCL_DEBUG=INFO
-
-env | grep SLURM
-
-# Launch example with torchrun
-torchrun \
-    --nnodes=$SLURM_JOB_NUM_NODES \
-    --nproc-per-node=${SLURM_GPUS_PER_TASK:-4} \
-    --rdzv-backend=c10d \
-    --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT \
-    nlp_example.py
+## Use scratch for huggingface cache
+#export HF_HOME=$SCRATCH/cache/huggingface
+#
+## Distributed training configuration
+#export MASTER_ADDR=$(scontrol show hostnames | head -n 1)
+#export MASTER_PORT=29507
+#
+#env | grep MASTER
+#
+## Other settings
+#export OMP_NUM_THREADS=8
+##export NCCL_DEBUG=INFO
+#
+#env | grep SLURM
+#
+## Launch example with torchrun
+#torchrun \
+#    --nnodes=$SLURM_JOB_NUM_NODES \
+#    --nproc-per-node=${SLURM_GPUS_PER_TASK:-4} \
+#    --rdzv-backend=c10d \
+#    --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT \
+#    nlp_example.py
 
 echo "SUCCESS"
